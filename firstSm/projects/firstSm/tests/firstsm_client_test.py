b2fdc193c59db6f6c171c01854ffb452cd4c2025
HarshilMinddeft <157495886+HarshilMinddeft@users.noreply.github.com>
2024-09-16 18:32:30 +0530

    Update firstsm_client_test.py

--- firstSm/projects/firstSm/tests/firstsm_client_test.py
+++ firstSm/projects/firstSm/tests/firstsm_client_test.py
@@ -3,46 +3,90 @@ import pytest
 from algokit_utils import get_localnet_default_account
 from algokit_utils.config import config
 from algosdk.v2client.algod import AlgodClient
+from algopy import *
 from algosdk.v2client.indexer import IndexerClient
+from algokit_utils.beta.algorand_client import (
+    AlgorandClient,
+    PayParams,
+    AssetCreateParams,
+)
+from algokit_utils.beta.account_manager import AddressAndSigner
+import algosdk
+from algosdk.atomic_transaction_composer import TransactionWithSigner
 
 from smart_contracts.artifacts.firstsm.firstsm_client import FirstsmClient
 
 
+# scope session means it needs to run once though it have multiple files tests
 @pytest.fixture(scope="session")
-def firstsm_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+def algorand() -> AlgorandClient:
+    """Get and algorandClient"""
+    return AlgorandClient.default_local_net()
+
+
+@pytest.fixture(scope="session")
+def dispenser(algorand: AlgorandClient) -> AddressAndSigner:
+    """Get dispenser account tokens"""
+    return algorand.account.dispenser()
+
+
+@pytest.fixture(scope="session")
+def creator(algorand: AlgorandClient, dispenser: AddressAndSigner) -> AddressAndSigner:
+    acct = algorand.account.random()
+
+    algorand.send.payment(
+        PayParams(sender=dispenser.address, receiver=acct.address, amount=10_000_000)
+    )
+    return acct
+
+
+@pytest.fixture(scope="session")
+def test_asset_id(creator: AddressAndSigner, algorand: AlgorandClient) -> int:
+    sent_txn = algorand.send.asset_create(
+        AssetCreateParams(sender=creator.address, total=10)
+    )
+    print(sent_txn)
+    return sent_txn["confirmation"]["asset-index"]
+
+
+@pytest.fixture(scope="session")
+def digital_marketplace_client(
+    algorand: AlgorandClient, creator: AddressAndSigner, test_asset_id: int
 ) -> FirstsmClient:
-    config.configure(
-        debug=True,
-        # trace_all=True,
-    )
-
+    """Iniciate application for using for our tests"""
     client = FirstsmClient(
-        algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
+        algod_client=algorand.client.algod,
+        sender=creator.address,
+        signer=creator.signer,
     )
-
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
-    )
+    client.create_create_application(unitaryPrice=0, assetId=test_asset_id)
     return client
 
 
-def test_says_hello(firstsm_client: FirstsmClient) -> None:
-    result = firstsm_client.hello(name="World")
-
-    assert result.return_value == "Hello, World"
-
-
-def test_simulate_says_hello_with_correct_budget_consumed(
-    firstsm_client: FirstsmClient, algod_client: AlgodClient
-) -> None:
-    result = (
-        firstsm_client.compose().hello(name="World").hello(name="Jane").simulate()
+def test_opt_in_to_asset(
+    digital_marketplace_client: FirstsmClient,
+    creator: AddressAndSigner,
+    test_asset_id: int,
+    algorand: AlgorandClient,
+):
+    # ensuer get_asset_information throws an error because app is not yet opted in
+    pytest.raises(
+        algosdk.error.AlgodHTTPError,
+        lambda: algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        ),
     )
-
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+    mbr_pay_txn = algorand.transactions.payment(
+        PayParams(
+            sender=creator.address,
+            receiver=digital_marketplace_client.app_address,
+            amount=200_000,
+            extra_fee=1_000,
+        )
+    )
+    digital_marketplace_client.opt_in_to_asset(
+        mbrPay=TransactionWithSigner(txn=mbr_pay_txn, signer=creator.signer),
+        transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[test_asset_id]
+        ),
+    )
