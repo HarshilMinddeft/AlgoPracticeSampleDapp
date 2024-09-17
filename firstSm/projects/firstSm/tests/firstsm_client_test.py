ff2e3488eb1454dc279782f430625255b3ae60c5
HarshilMinddeft <157495886+HarshilMinddeft@users.noreply.github.com>
2024-09-17 17:54:07 +0530

    Update firstsm_client_test.py

--- firstSm/projects/firstSm/tests/firstsm_client_test.py
+++ firstSm/projects/firstSm/tests/firstsm_client_test.py
@@ -9,6 +9,8 @@ from algokit_utils.beta.algorand_client import (
     AlgorandClient,
     PayParams,
     AssetCreateParams,
+    AssetOptInParams,
+    AssetTransferParams,
 )
 from algokit_utils.beta.account_manager import AddressAndSigner
 import algosdk
@@ -59,7 +61,7 @@ def digital_marketplace_client(
         sender=creator.address,
         signer=creator.signer,
     )
-    client.create_create_application(unitaryPrice=0, assetId=test_asset_id)
+    client.create_create_application(unitary_price=0, asset_id=test_asset_id)
     return client
 
 
@@ -69,13 +71,15 @@ def test_opt_in_to_asset(
     test_asset_id: int,
     algorand: AlgorandClient,
 ):
-    # ensuer get_asset_information throws an error because app is not yet opted in
+    # Ensure get_asset_information throws an error because app is not yet opted in
     pytest.raises(
         algosdk.error.AlgodHTTPError,
         lambda: algorand.account.get_asset_information(
             digital_marketplace_client.app_address, test_asset_id
         ),
     )
+
+    # Opt in to asset
     mbr_pay_txn = algorand.transactions.payment(
         PayParams(
             sender=creator.address,
@@ -84,9 +88,130 @@ def test_opt_in_to_asset(
             extra_fee=1_000,
         )
     )
-    digital_marketplace_client.opt_in_to_asset(
+    result = digital_marketplace_client.opt_in_to_asset(
         mbrPay=TransactionWithSigner(txn=mbr_pay_txn, signer=creator.signer),
         transaction_parameters=algokit_utils.TransactionParameters(
             foreign_assets=[test_asset_id]
         ),
+    )
+    assert result.confirmed_round
+
+    # Confirm that the application address now holds 0 of the asset
+    assert (
+        algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        )["asset-holding"]["amount"]
+        == 0
+    )
+
+
+def test_set_price(digital_marketplace_client: FirstsmClient):
+    result = digital_marketplace_client.set_price(unitary_price=3_00_000)
+    assert result.confirmed_round
+
+
+def test_deposit(
+    digital_marketplace_client: FirstsmClient,
+    creator: AddressAndSigner,
+    test_asset_id: int,
+    algorand: AlgorandClient,
+):
+    result = algorand.send.asset_transfer(
+        AssetTransferParams(
+            sender=creator.address,
+            receiver=digital_marketplace_client.app_address,
+            asset_id=test_asset_id,
+            amount=3,
+        )
+    )
+    assert result["confirmation"]
+    assert (
+        algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        )["asset-holding"]["amount"]
+        == 3
+    )
+
+
+def test_buy(
+    digital_marketplace_client: FirstsmClient,
+    creator: AddressAndSigner,
+    test_asset_id: int,
+    algorand: AlgorandClient,
+    dispenser: AddressAndSigner,
+):
+    # Set the price before attempting to buy
+    result = digital_marketplace_client.set_price(unitary_price=3_00_000)
+    assert result.confirmed_round
+
+    # for creation of new buyer
+    buyer = algorand.account.random()
+
+    # use the dispenser to fund buyer
+    algorand.send.payment(
+        PayParams(
+            sender=dispenser.address,
+            receiver=buyer.address,
+            amount=10_000_000,
+        )
+    )
+    # opt the buyer into asset
+    algorand.send.asset_opt_in(
+        AssetOptInParams(sender=buyer.address, asset_id=test_asset_id)
+    )
+
+    # form a transection for buying two asset (2*3_000_000)
+    buyer_payment_txn = algorand.transactions.payment(
+        PayParams(
+            sender=buyer.address,
+            receiver=digital_marketplace_client.app_address,
+            amount=2 * 3_300_000,
+            extra_fee=1_000,
+        )
+    )
+
+    result = digital_marketplace_client.buy(
+        buyerTxn=TransactionWithSigner(txn=buyer_payment_txn, signer=buyer.signer),
+        quantity=2,
+        transaction_parameters=algokit_utils.TransactionParameters(
+            sender=buyer.address,
+            signer=buyer.signer,
+            # for telling evm about asset call
+            foreign_assets=[test_asset_id],
+        ),
+    )
+
+    assert result.confirmed_round
+    assert (
+        algorand.account.get_asset_information(buyer.address, test_asset_id)[
+            "asset-holding"
+        ]["amount"]
+        == 2
+    )
+
+
+def test_delete_application(
+    digital_marketplace_client: FirstsmClient,
+    creator: AddressAndSigner,
+    test_asset_id: int,
+    algorand: AlgorandClient,
+    dispenser: AddressAndSigner,
+):
+    before_call_amount = algorand.account.get_information(creator.address)["amount"]
+
+    result = digital_marketplace_client.delete_delete_application(
+        transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[test_asset_id],
+        )
+    )
+    assert result.confirmed_round
+
+    after_call_amount = algorand.account.get_information(creator.address)["amount"]
+
+    assert after_call_amount - before_call_amount == (2 * 3_300_000) + 200_000 - 3_000
+    assert (
+        algorand.account.get_asset_information(creator.address, test_asset_id)[
+            "asset-holding"
+        ]["amount"]
+        == 8
     )
