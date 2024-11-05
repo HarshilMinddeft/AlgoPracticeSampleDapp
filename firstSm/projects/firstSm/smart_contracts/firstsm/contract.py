40872332ebd6fbaf48fb7a23d1614105657689b8
HarshilMinddeft <157495886+HarshilMinddeft@users.noreply.github.com>
2024-11-05 17:48:09 +0530

    Update contract.py

--- firstSm/projects/firstSm/smart_contracts/firstsm/contract.py
+++ firstSm/projects/firstSm/smart_contracts/firstsm/contract.py
@@ -1,84 +1,132 @@
 from algopy import *
+from algopy import arc4
 
-# from algopy.arc4 import abimethod
+
+# Define StreamData outside of the Steam class
+class StreamData(arc4.Struct):
+    streamRate: arc4.UInt64  # MicroAlgos per second
+    startTime: arc4.UInt64
+    endTime: arc4.UInt64  # Time when all funds will be streamed
+    withdrawnAmount: arc4.UInt64
+    recipient: arc4.Address  # Recipient account
+    balance: arc4.UInt64  # Track contract balance for each stream
+    isStreaming: arc4.Bool  # Track streaming status
+    last_withdrawal_time: arc4.UInt64  # Last withdrawal time
 
 
-# ARC4Contract is algorand standard it make contract more intractable, automatically handel routes
-class Firstsm(ARC4Contract):
-    # @abimethod()
-    # These two types are set to globel types
-    asset_id: UInt64
-    unitary_price: UInt64
+class TSteam(ARC4Contract):
+    def __init__(self) -> None:
+        self.streams = BoxMap(UInt64, StreamData, key_prefix="")
+        self.streamCounter = UInt64(0)
 
-    # create the app
-    # This abimethode is used for call contract user can call this function
-    # we used create = "required" this will call this function with contract creation
-    #  NoOp (general operation) transaction, meaning it's not a special type of action like creating, deleting, or updating the application.
-    @arc4.abimethod(allow_actions=["NoOp"], create="require")
-    # This function is for creating contract
-    def createApplication(self, asset_id: Asset, unitary_price: UInt64) -> None:
-        self.asset_id = asset_id.id
-        self.unitary_price = unitary_price
+    # Start a new stream
+    @arc4.abimethod(allow_actions=["NoOp"])
+    def startStream(self, recipient: Account, rate: UInt64, amount: UInt64) -> None:
+        assert Txn.sender == Global.creator_address, "Only creator can start a stream"
+        assert rate > UInt64(0), "Stream rate must be greater than 0"
+        assert amount > UInt64(0), "Stream amount must be greater than 0"
 
-    # update the listing price.
-    @arc4.abimethod
-    # This function is for updating the asset price.
-    def setPrice(self, unitary_price: UInt64) -> None:
-        # check is for onlyOwner can call this external function.
-        assert Txn.sender == Global.creator_address
-        self.unitary_price = unitary_price
+        # Increment the stream counter to get a unique stream ID
+        self.streamCounter += UInt64(1)
+        newStreamId = self.streamCounter
 
-    # opt into the asset that will be sold
-    # This function is for to check smartcontract account should be optend in to the asset that seller wanted to sell
-    @arc4.abimethod
-    def opt_in_to_asset(self, mbrPay: gtxn.PaymentTransaction) -> None:
-        assert Txn.sender == Global.creator_address
-        assert not Global.current_application_address.is_opted_in(Asset(self.asset_id))
+        # Calculate the stream end time: total time to stream all the amount = amount / rate
+        stream_duration = amount // rate
+        end_time = Global.latest_timestamp + stream_duration
 
-        assert mbrPay.receiver == Global.current_application_address
-        assert mbrPay.amount == Global.min_balance + Global.asset_opt_in_min_balance
-        itxn.AssetTransfer(
-            xfer_asset=self.asset_id,
-            asset_receiver=Global.current_application_address,
-            asset_amount=0,
+        # Create a new StreamData object
+        self.streams[newStreamId] = StreamData(
+            streamRate=arc4.UInt64(rate),
+            startTime=arc4.UInt64(Global.latest_timestamp),
+            endTime=arc4.UInt64(end_time),
+            withdrawnAmount=arc4.UInt64(0),
+            recipient=arc4.Address(recipient),
+            balance=arc4.UInt64(amount),
+            isStreaming=arc4.Bool(True),
+            last_withdrawal_time=arc4.UInt64(0),
+        )
+
+    # Calculate the total streamed amount for a specific stream
+    @subroutine
+    def _calculateStreamedAmount(self, streamId: UInt64) -> UInt64:
+        # assert streamId < self.streamCounter, "Invalid stream ID"
+        stream = self.streams[streamId].copy()
+        current_time = Global.latest_timestamp
+
+        if current_time >= stream.endTime:
+            total_streamed = stream.balance.native + stream.withdrawnAmount.native
+        else:
+            elapsed_time = current_time - stream.startTime.native
+            total_streamed = elapsed_time * stream.streamRate.native
+
+        # Ensure total_streamed does not exceed initial amount
+        initial_amount = stream.streamRate.native * (
+            stream.endTime.native - stream.startTime.native
+        )
+        if total_streamed > initial_amount:
+            total_streamed = initial_amount
+
+        return total_streamed - stream.withdrawnAmount.native
+
+    # Withdraw funds for the recipient of a specific stream
+    @arc4.abimethod(allow_actions=["NoOp"])
+    def withdraw(self, streamId: UInt64) -> None:
+        # assert streamId < self.streamCounter, "Invalid stream ID"
+        stream = self.streams[streamId].copy()
+        # assert Txn.sender == stream.recipient, "Only the recipient can withdraw"
+
+        available_amount = self._calculateStreamedAmount(streamId)
+        balance = stream.balance
+
+        if available_amount > balance:
+            amount_to_withdraw = balance
+        else:
+            amount_to_withdraw = arc4.UInt64(available_amount)
+
+        # assert amount_to_withdraw > arc4.UInt64(0), "No available funds to withdraw"
+
+        # Update the stream's withdrawn amount and balance
+        updated_withdrawn = stream.withdrawnAmount.native + amount_to_withdraw.native
+        updated_balance = stream.balance.native - amount_to_withdraw.native
+        updated_stream = StreamData(
+            streamRate=stream.streamRate,
+            startTime=stream.startTime,
+            endTime=stream.endTime,
+            withdrawnAmount=arc4.UInt64(updated_withdrawn),
+            recipient=stream.recipient,
+            balance=arc4.UInt64(updated_balance),
+            isStreaming=stream.isStreaming,
+            last_withdrawal_time=arc4.UInt64(Global.latest_timestamp),
+        )
+
+        # Update the stream data in the DynamicArray
+        self.streams[streamId] = updated_stream.copy()
+
+        # Submit inner transaction for payment
+        itxn.InnerTransaction(
+            sender=Global.current_application_address,
+            receiver=stream.recipient.native,
+            amount=amount_to_withdraw.native,
+            note=b"Withdrawal from stream",
+            type=TransactionType.Payment,
         ).submit()
 
-    # buy the asset
-    @arc4.abimethod
-    def buy(
-        self,
-        buyerTxn: gtxn.PaymentTransaction,
-        quantity: UInt64,
-    ) -> None:
-        # check for buyer is buying asset at price that seller decided, not giving free stuff
-        assert self.unitary_price != UInt64(0)
-        decoded_quantity = quantity
-        # check for account that call buy method is same that sent paymentTransection
-        assert buyerTxn.sender == Txn.sender
+    @arc4.abimethod(allow_actions=["NoOp"])
+    def getStreamData(self, streamId: UInt64) -> StreamData:
 
-        assert buyerTxn.receiver == Global.current_application_address
-        assert buyerTxn.amount == self.unitary_price * decoded_quantity
-        # for sending asset
-        itxn.AssetTransfer(
-            xfer_asset=self.asset_id,
-            asset_receiver=Txn.sender,
-            asset_amount=decoded_quantity,
-        ).submit()
+        assert streamId <= self.streamCounter, "Invalid stream ID"
 
-    # delete the application
-    @arc4.abimethod(allow_actions=["DeleteApplication"])
-    def deleteApplication(self) -> None:
-        assert Txn.sender == Global.creator_address
+        # Retrieve the stream data
+        stream_data = self.streams[streamId].copy()
 
-        itxn.AssetTransfer(
-            xfer_asset=self.asset_id,
-            asset_receiver=Global.creator_address,
-            asset_amount=0,
-            asset_close_to=Global.creator_address,
-        ).submit()
-
-        itxn.Payment(
-            receiver=Global.creator_address,
-            amount=0,
-            close_remainder_to=Global.creator_address,
-        ).submit()
+        # Return the stream data
+        return StreamData(
+            streamRate=stream_data.streamRate,
+            startTime=stream_data.startTime,
+            endTime=stream_data.endTime,
+            withdrawnAmount=stream_data.withdrawnAmount,
+            recipient=stream_data.recipient,
+            balance=stream_data.balance,
+            isStreaming=stream_data.isStreaming,
+            last_withdrawal_time=stream_data.last_withdrawal_time,
+        )
